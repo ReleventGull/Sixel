b4f235aad40286a37d43691627d412335e88afb0
Jaron <jaronchretien@yahoo.com>
2023-05-17 17:40:49 -0500

    creats and adds the table for rendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-
+import { useState } from "react"
+import Squares from "./Squares"
 
 
 const App = () => {
+    const [rowsCol, setRolCol] = useState(Array(6).fill('1'))
     return (
-        <h2>Hello World</h2>
+        <>
+            <div className="table">
+            {rowsCol.map(it => 
+                <div className="row">
+                    {rowsCol.map(it => 
+                    <div className="square">
+
+                    </div>)}
+                </div>
+            )}
+            </div>
+        </>
     )
 }
 
