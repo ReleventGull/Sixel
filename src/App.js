8f64cea2ad3cacaa2cd2740b00fd1ca9bd45aea2
Jaron <jaronchretien@yahoo.com>
2023-05-18 17:05:44 -0500

    feat(keyboard): Adds the keyboard to the mix

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Squares from "./Squares"
 
 const App = () => {
     const [rowsCol, setRolCol] = useState(Array(6).fill('1'))
+    const [keyboard, setKeyboard] = useState([`${'Q W E R T Y U I O P'}`.split(' '), `A S D F G H J K L`.split(' '), 'ENTER Z X C V B N M BACKSPACE'.split(' ')])
+    console.log(keyboard)
     return (
         <>
             <div className="table">
@@ -11,10 +13,20 @@ const App = () => {
                 <div className="row">
                     {rowsCol.map(it => 
                     <div className="square">
-
                     </div>)}
                 </div>
             )}
+            </div>
+            <div className="keyboard">
+                    {keyboard.map(it =>
+                    <div className="keyRow"> 
+                        {it.map(it2 => 
+                            <div className="letterKey"><h1>{it2}</h1></div>
+                            
+                            )}
+
+                    </div>
+                        )}
             </div>
         </>
     )
