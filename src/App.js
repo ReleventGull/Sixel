8d4f4fa00330b305aac4111a84289696f8f21dca
cusmoritz <cus.moritz@gmail.com>
2023-05-25 19:47:01 -0600

    feat: Trying to seperate working row from empty rows

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { useState } from "react"
-import Squares from "./Squares"
+import { Squares } from "./Squares"
 
 
 const App = () => {
     const [rowsCol, setRolCol] = useState(Array(6).fill('1'))
-    const [keyboard, setKeyboard] = useState([`${'Q W E R T Y U I O P'}`.split(' '), `A S D F G H J K L`.split(' '), 'ENTER Z X C V B N M BACKSPACE'.split(' ')])
-    console.log(keyboard)
+    const [keyboard, setKeyboard] = useState([`${'Q W E R T Y U I O P'}`.split(' '), `A S D F G H J K L`.split(' '), 'Z X C V B N M'.split(' ')])
+    const [wordGuess, setWordGuess] = useState([]);
+    console.log('word guess', wordGuess)
     return (
         <div className="mainContainer">
 
@@ -15,6 +16,8 @@ const App = () => {
                 <div className="row">
                     {rowsCol.map(it => 
                     <div className="square">
+                       <Squares wordGuess={wordGuess}/>
+
                     </div>)}
                 </div>
             )}
@@ -23,7 +26,8 @@ const App = () => {
                     {keyboard.map(it =>
                     <div className="keyRow"> 
                         {it.map(it2 => 
-                            <div className="letterKey"><h3 >{it2}</h3 ></div>
+                            <div className="letterKey" ><h3 onClick={(event) => 
+                                setWordGuess((previous) => [...previous, event.target.textContent])}>{it2}</h3 ></div>
                             
                             )}
 
---
+++ src/Squares.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export const Squares = ({wordGuess}) => {
+
+    return (
+        <div >
+            <p>{wordGuess[0]}</p>
+        </div>
+    )
+}
+
